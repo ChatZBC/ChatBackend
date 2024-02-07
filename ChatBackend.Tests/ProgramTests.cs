14ee466f5c2dfe0e37a1740ca8dbec7fc5444ab6
Drag-NDrop <110815341+Drag-NDrop@users.noreply.github.com>
2024-02-07 18:39:03 +0100

    Skrevet 5 tests til ProgramTests.cs
    ShouldAddSignalRService()
    ShouldUseEndpoints()
    ShouldUseCors()
    ShouldMapChatHub()
    ShouldMapControllers()

--- ChatBackend.Tests/ProgramTests.cs
+++ ChatBackend.Tests/ProgramTests.cs
@@ -14,17 +14,127 @@ namespace ChatBackend.Tests
     {
 
         [Fact]
-    public void ShouldAddSignalRService() {
+        // test this:  builder.Services.AddSignalR();
+        public void ShouldAddSignalRService()
+        {
 
-                // Arrange
-                string[] args = new string[] { };   
-                var builder = WebApplication.CreateBuilder(args);
+            // Arrange
+            string[] args = new string[] { };
+            var builder = WebApplication.CreateBuilder(args);
 
-                // Act
-                builder.Services.AddSignalR();
+            // Act
+            builder.Services.AddSignalR();
 
-                // Assert
-                Assert.NotNull(builder);
+            // Assert
+            Assert.NotNull(builder);
+        }
+
+        [Fact]
+        // test this: Builder services adding
+        public void ShouldUseEndpoints()
+        {
+
+            // Arrange
+            string[] args = new string[] { };
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Act
+            builder.Services.AddSignalR();
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "ChatBackend", Version = "v1" });
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Assert
+            Assert.NotNull(builder);
+        }
+
+        [Fact]
+        // test this:  app.UseCors(settings => settings.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+        public void ShouldUseCors()
+        {
+
+            // Arrange
+            string[] args = new string[] { };
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Act
+            builder.Services.AddSignalR();
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "ChatBackend", Version = "v1" });
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            app.UseCors(settings => settings.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            // Assert
+            Assert.NotNull(app);
+        }
+
+        [Fact]
+        // test this:  app.MapHub<ChatHub>("/chatHub");
+        public void ShouldMapChatHub()
+        {
+
+            // Arrange
+            string[] args = new string[] { };
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Act
+            builder.Services.AddSignalR();
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "ChatBackend", Version = "v1" });
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            app.UseCors(settings => settings.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.MapHub<ChatHub>("/chatHub");
+
+            // Assert
+            Assert.NotNull(app);
+        }
+
+        [Fact]
+        // test this:  app.MapControllers();
+        public void ShouldMapControllers()
+        {
+
+            // Arrange
+            string[] args = new string[] { };
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Act
+            builder.Services.AddSignalR();
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "ChatBackend", Version = "v1" });
+            });
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            app.UseCors(settings => settings.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.MapHub<ChatHub>("/chatHub");
+            app.MapControllers();
+
+            // Assert
+            Assert.NotNull(app);
         }
     }
 }
