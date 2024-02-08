a311e9c9945875623dd3b6809cf284282c6bc951
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2024-02-08 08:41:27 +0100

    Implemented input validation interface and composite input validator pattern for input validation.

---
+++ ChatBackend/Security/InputValidation/InputValidationComposite.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ChatBackend.Security.InputValidation
+{
+    public class InputValidationComposite : IInputValidator
+    {
+        public bool Validate(string input)
+        {
+            if (InputValidators.Count == 0)
+            {
+                throw new NotImplementedException("Unexpected usage of InputValidationComposite: No input validators added.");
+            }
+
+            foreach (IInputValidator inputValidator in InputValidators)
+            {
+                if (!inputValidator.Validate(input))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void AddInputValidator(IInputValidator inputValidator)
+        {
+            InputValidators.Add(inputValidator);
+        }
+
+        public InputValidationComposite()
+        {
+            InputValidators = new List<IInputValidator>();
+        }
+
+        public InputValidationComposite(List<IInputValidator> inputValidators)
+        {
+            InputValidators = inputValidators;
+        }
+
+        private List<IInputValidator> InputValidators;
+    }
+}
