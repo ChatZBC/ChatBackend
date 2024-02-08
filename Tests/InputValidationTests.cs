f0ebb5dbc78403733bc8988b4b45f70dcc230c1b
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2024-02-08 09:13:19 +0100

    Added input validation test for empty composite object

--- Tests/InputValidationTests.cs
+++ Tests/InputValidationTests.cs
@@ -1,11 +1,16 @@
+using ChatBackend.Security.InputValidation;
+
 namespace Tests
 {
     public class InputValidationTests
     {
         [Fact]
-        public void Validate_Throws()
+        public void Validate_ExceptionWhenCompositeEmpty()
         {
-
+            InputValidationComposite comp = new InputValidationComposite();
+            Assert.Throws<InvalidOperationException>(() => comp.Validate(""));
         }
+
+
     }
 }
